ef2046f951e917d2b0091963ef4d9f7c7de15315
KucinyYs <yeshi.shoham@gmail.com>
2014-11-01 08:38:05 -0700

    games ratio

--- draw_kingdom/lib/strategies/ds_teams_draw_ratio_strategy.rb
+++ draw_kingdom/lib/strategies/ds_teams_draw_ratio_strategy.rb
@@ -4,7 +4,6 @@ class DSTeamsDrawRatioStrategy < DSBaseStrategy
 
 
   def getGrade
-    future_games = @all_team_games.select{|game| game_date > @due_to_date}.sort {|x,y| x.game_date <=> y.game_date}
     future_games_grader = DSFutureGamesGrader.new(@all_team_games,@due_to_date)
     grade = future_games_grader.getGrade
     return normalizeGrade(grade,10)
