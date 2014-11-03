e0281a2d446d30b110e40bf9669d357808c6efd7
KucinyYs <yeshi.shoham@gmail.com>
2014-11-03 10:00:53 -0800

    adding test vectors

--- draw_kingdom/lib/components/ds_file_reader.rb
+++ draw_kingdom/lib/components/ds_file_reader.rb
@@ -98,8 +98,9 @@ class DSFileReader
   # return array of all games form file
   def initDataFromFiles
 
-    all_games_url = "../DB/" + @url_file_name + "_games.drk"
-    all_teams_url = "../DB/" + @url_file_name + "_teams.drk"
+    relative_path = File.dirname(File.realpath(__FILE__)) + "/../"
+    all_games_url = relative_path + "../DB/" + @url_file_name + "_games.drk"
+    all_teams_url = relative_path + "../DB/" + @url_file_name + "_teams.drk"
 
     if (File.exist?(all_games_url) && File.exist?(all_teams_url))
 
@@ -114,7 +115,7 @@ class DSFileReader
       @teams_names_dictionary = {}
       @games_array = Array.new()
 
-      File.open("../stats/" + @url_file_name).read.each_line do |current_file|
+      File.open(relative_path + "../stats/" + @url_file_name).read.each_line do |current_file|
 
         # Creating teams dictionary
         if (current_file.start_with?("#"))
--- draw_kingdom/spec/lib/strategies/teams_draw_ration_stategy_spec.rb
+++ draw_kingdom/spec/lib/strategies/teams_draw_ration_stategy_spec.rb
@@ -1,46 +1,25 @@
 require 'rspec'
-require_relative "../../../lib/strategies/ds_future_games_grader"
+require_relative "../../../lib/strategies/ds_teams_draw_ratio_strategy"
 require_relative "../../../lib/model/ds_team"
 require_relative "../../../lib/model/ds_game"
+require_relative "../../../lib/components/ds_file_reader"
 
-hapoel = DSTeam.new("hapoel")
-maccabi = DSTeam.new("maccabi")
-haifa = DSTeam.new("haifa")
-beitar = DSTeam.new("beitar")
-ashdod = DSTeam.new("ashdod")
-tubruk = DSTeam.new("tubruk")
+def getTestedTeam
+  file_reader.teamsHash.values.select { |team| team.team_name.eql? RSpec.current_example.description }[0]
+end
 
-# todo use file test vector for all specs instead of hard coded.
-allGamesArray = [
-    DSGame.new(Date.new(2010, 1, 1), hapoel, haifa, 1, 0, nil, "Israel"),
-    DSGame.new(Date.new(2010, 1, 2), hapoel, ashdod, 1, 0, nil, "Israel"),
-    DSGame.new(Date.new(2010, 1, 3), hapoel, maccabi, 0, 0, nil, "Israel"),
-    DSGame.new(Date.new(2010, 1, 4), hapoel, beitar, 0, 1, nil, "Israel"),
-    DSGame.new(Date.new(2010, 1, 5), hapoel, ashdod, 0, 0, nil, "Israel"),
-    DSGame.new(Date.new(2010, 1, 6), hapoel, haifa, 0, 0, nil, "Israel"),
-    DSGame.new(Date.new(2010, 1, 7), hapoel, haifa, 0, 0, nil, "Israel"),
-    DSGame.new(Date.new(2010, 1, 8), hapoel, maccabi, 0, 0, nil, "Israel"),
-    DSGame.new(Date.new(2010, 1, 9), hapoel, ashdod, 0, 0, nil, "Israel"),
-    DSGame.new(Date.new(2010, 1, 10), hapoel, maccabi, 0, 0, nil, "Israel"),
-    DSGame.new(Date.new(2010, 1, 11), hapoel, tubruk, 0, 0, nil, "Israel"),
-    DSGame.new(Date.new(2010, 1, 12), hapoel, beitar, 0, 0, nil, "Israel")
-]
+describe DSTeamsDrawRatioStrategy do
 
-describe DSFutureGamesGrader do
-  it 'should give 0 with no previous game' do
-    grade = DSFutureGamesGrader.new(allGamesArray[0..10], Date.new(2010, 1, 10),1).getGrade
-    grade.should == 0.0
+  let(:file_reader) {file_reader = DSFileReader.new("test_vectors")}
+
+  it "hapoel" do
+    subject.loadGamesAndTeam(file_reader, getTestedTeam, Date.new(2014,1,10), nil, 5)
+    subject.getGrade.should == 5.0
   end
-  it 'should give 1.0 with only draws'do
-    grade = DSFutureGamesGrader.new(allGamesArray[0..7], Date.new(2010, 1, 7),1).getGrade
-    grade.should == 1.0
+
+  it "maccabi" do
+    subject.loadGamesAndTeam(file_reader, getTestedTeam, Date.new(2014,1,9), nil, 5)
+    subject.getGrade.should == 10.0
   end
-  it 'should give 0.0 with only non-draws'do
-    grade = DSFutureGamesGrader.new(allGamesArray[0..5], Date.new(2010, 1, 5),1).getGrade
-    grade.should == 0.0
-  end
-  it 'should give 0.5 with draws in 50% of the games'do
-    grade = DSFutureGamesGrader.new(allGamesArray[0..6], Date.new(2010, 1, 6),1).getGrade
-    grade.should == 0.5
-  end
+
 end
