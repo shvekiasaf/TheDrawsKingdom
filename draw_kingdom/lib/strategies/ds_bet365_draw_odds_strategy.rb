59e2bb8a6b9957853e0a3f9169b1a1894f99df3d
KucinyYs <yeshi.shoham@gmail.com>
2015-01-24 02:22:28 -0800

    reverting after CR with Asaf

--- draw_kingdom/lib/strategies/ds_bet365_draw_odds_strategy.rb
+++ draw_kingdom/lib/strategies/ds_bet365_draw_odds_strategy.rb
@@ -2,8 +2,6 @@
 
 require_relative "../strategies/ds_base_strategy"
 
-# todo what does this strategy return? it currently returns (sum of draw odds for team) / (number of games)
-# todo why not just return the value of current_game.b365_draw_odds
 class DSBet365DrawOddsStrategy < DSBaseStrategy
 
   def initialize(since)
@@ -21,15 +19,15 @@ class DSBet365DrawOddsStrategy < DSBaseStrategy
     odds_summary  = 0
     game_index    = 0
 
-    # todo why?? aren't we already getting this from @all_team_games?
-    # todo why Date.today??
-    # games_in_range = @all_team_games.select{|current_game| current_game.game_date.to_datetime > Date.today-@since }
+    games_in_range = @all_team_games.select{|current_game| current_game.game_date.to_datetime > Date.today-@since }
 
-    # todo if anything, the query should be
-    games_in_range = @all_team_games.select{|current_game|(not current_game.b365_draw_odds.nil?) and (current_game.b365_draw_odds.to_f.zero?)}
     games_in_range.each do |current_game|
-      game_index += 1
-      odds_summary += current_game.b365_draw_odds.to_f
+
+      if (not current_game.b365_draw_odds.nil?)
+
+        game_index += 1
+        odds_summary += current_game.b365_draw_odds.to_f
+      end
     end
 
     if (game_index == 0)
@@ -38,14 +36,8 @@ class DSBet365DrawOddsStrategy < DSBaseStrategy
 
       odds_avg = odds_summary / game_index
 
-      return normalizeGrade(odds_avg, 8.0)
+      DSHelpers.reverse_normalize_value(odds_avg,8.0,100.0)
     end
-  end
-  # todo don't use this. use the dshelpers.reverse...
-  def normalizeGrade(grade, range)
-    normalizeGrade = super(grade, range)
-
-    return (normalizeGrade - 100).abs
   end
 
 end
